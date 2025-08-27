fee08b22e0287c62da55ed8b1a739feabaf50506
rafaeldicarlantonio <rafael.dicarlantonio@societyforuapstudies.org>
2025-08-27 21:05:09 +0200

    Update openai_client.py

--- vendors/openai_client.py
+++ vendors/openai_client.py
@@ -3,4 +3,4 @@ from openai import OpenAI
 
 client = OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
 EMBED_MODEL = os.getenv("OPENAI_EMBED_MODEL", "text-embedding-3-small")
-CHAT_MODEL  = os.getenv("OPENAI_CHAT_MODEL",  "gpt-4.1-mini")
+CHAT_MODEL  = os.getenv("OPENAI_CHAT_MODEL",  "gpt-5")
