de020f05632f0c60b3586d4242127a658c71bd79
rafaeldicarlantonio <rafael.dicarlantonio@societyforuapstudies.org>
2025-09-01 20:10:24 +0200

    Update pinecone_client.py

--- vendors/pinecone_client.py
+++ vendors/pinecone_client.py
@@ -2,4 +2,4 @@ import os
 from pinecone import Pinecone
 
 pc = Pinecone(api_key=os.getenv("PINECONE_API_KEY"))
-INDEX = pc.Index(os.getenv("PINECONE_INDEX", "memories"))
+INDEX = pc.Index(os.getenv("PINECONE_INDEX", "uap-kb"))
